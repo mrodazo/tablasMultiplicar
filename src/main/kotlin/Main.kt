2d03be59e9aabbc842ad78142723742f5644c3b0
mrodazo617 <mrodazo617@g.educaand.es>
2024-01-08 19:48:47 +0100

    Versión tabla multiplicación

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+/**
+ * Solicita un número entero en un rango.
+ *
+ * @param min Int - valor mínimo
+ * @param max Int - valor máximo
+ *
+ * @return Int - número entero válido dentro del rango especificado
+ */
+fun pedirNum (num: Int): Int{
+
+
+}
+
+/**
+ * Realiza una pregunta para contestar con s/si ó n/no
+ *
+ * @param text String - Texto de la pregunta
+ *
+ * @return Boolean - true/false dependiendo de la respuesta válida a la pregunta
+ */
+fun pregunta (respuesta: String): Boolean {
+    println("¿Desea generar otra tabla de multiplicación? (s/n)")
+    respuesta
+
+}
+
+fun main() {
+    //TODO: Solicitar la introducción de un número entre 1 y 100 y mostrar su table de multiplicar...
+    //Hasta que se responda negativamente a la pregunta "¿Desea generar otra tabla de multiplicación? (s/n)"
+}
